4a911d4bf064bbb7e185df86767355d68d0878d3
Masoud S.R <amir.sepehr72@gmail.com>
2023-05-03 15:45:08 +0330

    digital currency app started

---
+++ arzdigital/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import Coins from './components/Coins'
+
+function App() {
+  return (
+     <Coins />
+  )
+}
+
+export default App
---
+++ arzdigital/src/components/Coins.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Coin from './Coin';
+
+const Coins = () => {
+    return (
+            <Coin />
+    );
+};
+
+export default Coins;
