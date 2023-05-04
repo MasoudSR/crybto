6f22db3e2b324ceb6c186b320c6a37742ac4a7a4
Masoud S.R <amir.sepehr72@gmail.com>
2023-05-04 12:31:45 +0330

    header added

--- arzdigital/src/App.jsx
+++ arzdigital/src/App.jsx
@@ -5,13 +5,17 @@ import './App.css'
 // components
 import Coins from './components/Coins'
 import CoinDetails from "./components/CoinDetails"
+import Header from './components/Header'
 
 function App() {
   return (
+    <>
+    <Header />
     <Routes>
       <Route path="/" element={<Coins />} />
       <Route path='/:id' element={<CoinDetails />} />
     </Routes>
+    </>
   )
 }
 
