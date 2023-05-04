dd96bb4c3185f3c071281f5cc8251b78b5afedf1
Masoud S.R <amir.sepehr72@gmail.com>
2023-05-04 12:25:14 +0330

    styles and error handler improved

--- arzdigital/src/components/CoinDetails.jsx
+++ arzdigital/src/components/CoinDetails.jsx
@@ -1,25 +1,26 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./CoinDetails.module.css"
 
 const CoinDetails = () => {
     const [coinData, setCoinData] = useState({});
     const params = useParams();
+    const [error, setError] = useState("")
 
     useEffect(() => {
         axios.get(`https://api.coingecko.com/api/v3/coins/${params.id}`)
             .then((res) => {
                 setCoinData(res.data);
             }).catch(err => {
-                console.log(err)
+                setError(err)
             })
     }
         , []
     )
 
     if (Object.keys(coinData).length == 0) {
-        return (<h1>Loading</h1>)
+        return (!error ? <h1 className={styles.alert}>Loading</h1> : <h1 className={styles.alert}>${error.message}</h1>)
     } else {
         return (
             <div className={styles.container}>
@@ -27,7 +28,7 @@ const CoinDetails = () => {
                 <h1>{coinData.name}</h1>
                 <h1>Price:{coinData.market_data.current_price.usd}$</h1>
                 <h3>Last Update:{coinData.last_updated}</h3>
-                <p>{coinData.description.en}</p>
+                <p dangerouslySetInnerHTML={{ __html: coinData.description.en }}></p>
                 <a href={coinData.links.homepage}>{coinData.links.homepage}</a>
             </div>)
     };
