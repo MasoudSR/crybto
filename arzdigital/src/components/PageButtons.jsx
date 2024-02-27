065f5899f3d7d28f5b5f3d9794ae3c9d03e3941b
Masoud S.R <amir.sepehr72@gmail.com>
2024-02-27 16:42:01 +0330

    page button styles improved for mobile view

--- arzdigital/src/components/PageButtons.jsx
+++ arzdigital/src/components/PageButtons.jsx
@@ -9,16 +9,11 @@ const PageButtons = ({ pageNumber, setPageNumber }) => {
         <div className={styles.container} >
             <button className={styles.pageNumber} disabled={pageNumber == 1 && true} onClick={() => { setPageNumber(pageNumber - 1) }}><MdKeyboardArrowLeft size={20} /></button>
 
-            {pageNumber > 3 && <h2 onClick={() => { setPageNumber(1) }} className={styles.pageNumber}>1</h2>}
-            {pageNumber > 3 && <h2 className={styles.threeDots}><PiDotsThreeOutlineThin /></h2>}
-            {pageNumber > 2 && <h2 onClick={() => { setPageNumber(pageNumber - 2) }} className={styles.pageNumber}>{pageNumber - 2}</h2>}
-            {pageNumber > 1 && <h2 onClick={() => { setPageNumber(pageNumber - 1) }} className={styles.pageNumber}>{pageNumber - 1}</h2>}
-            {!pageNumber == 1 && <h2 className={styles.pageNumber}>1</h2>}
-            <h2 className={styles.currentPage}>{pageNumber}</h2>
-            <h2 onClick={() => { setPageNumber(pageNumber + 1) }} className={styles.pageNumber}>{pageNumber + 1}</h2>
-            <h2 onClick={() => { setPageNumber(pageNumber + 2) }} className={styles.pageNumber}>{pageNumber + 2}</h2>
-            <h2 className={styles.threeDots}><PiDotsThreeOutlineThin /></h2>
-            <h2 onClick={() => { setPageNumber(pageNumber + 5) }} className={styles.pageNumber}>{pageNumber + 5}</h2>
+            {pageNumber > 1 && <button onClick={() => { setPageNumber(1) }} className={styles.pageNumber}>1</button>}
+            {pageNumber > 2 && <h2 className={styles.threeDots}><PiDotsThreeOutlineThin /></h2>}
+            <button className={styles.currentPage}>{pageNumber}</button>
+            <button onClick={() => { setPageNumber(pageNumber + 1) }} className={styles.pageNumber}>{pageNumber + 1}</button>
+            <button onClick={() => { setPageNumber(pageNumber + 2) }} className={styles.pageNumber}>{pageNumber + 2}</button>
 
             <button className={styles.pageNumber} onClick={() => { setPageNumber(pageNumber + 1) }}><MdKeyboardArrowRight size={20} /></button>
         </div>
