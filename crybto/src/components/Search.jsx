01ab629b2fdb6917cff829e17e750b544047ce81
Masoud S.R <amir.sepehr72@gmail.com>
2024-03-04 12:24:51 +0330

    some search problems fixed

--- crybto/src/components/Search.jsx
+++ crybto/src/components/Search.jsx
@@ -4,29 +4,31 @@ import { useState } from "react"
 import CoinSearchTile from "./modules/CoinSearchTile"
 import styles from "./Search.module.css"
 import CoinSearching from "./Loading/CoinSearching"
+import { DebounceInput } from "react-debounce-input"
 
 
 function Search({ searchBar }) {
     const [search, setSearch] = useState("")
 
     const fetchSearchedCoin = () => axios.get(`https://api.coingecko.com/api/v3/search?query=${search}`)
-    const { data, isFetching, isError, refetch } = useQuery({ queryKey: ["searchedCoins", search], queryFn: fetchSearchedCoin, enabled: false, refetchOnMount: false, refetchOnWindowFocus: false })
+    const { data, isLoading, isError } = useQuery({ queryKey: ["searchedCoins", search], queryFn: fetchSearchedCoin, refetchOnMount: false, refetchOnWindowFocus: false, enabled: search.length > 0, staleTime: 5 * 60 * 1000 })
 
     const searchHandler = (e) => {
-        setTimeout(refetch, 400)
         setSearch(e.target.value)
     }
 
 
     return (
         <div className={styles.container}>
-            <input type="text" value={search} onChange={searchHandler} placeholder="Search" className={styles.searchBox} />
-            <div className={styles.searchResults}>
-                {isError && <p className={styles.resultTile}>Error in receiving data, try again</p>}
-                {isFetching && <CoinSearching />}
-                {data && data.data.coins.length === 0 && <p className={styles.resultTile}>Coin not found</p>}
-                {search.length > 0 && searchBar && data && data.data.coins.slice(0, 5).map(coin => <CoinSearchTile key={coin.id} coin={coin} />)}
-            </div>
+            <DebounceInput debounceTimeout={800} onChange={searchHandler} className={styles.searchBox} />
+            {searchBar &&
+                <div className={styles.searchResults}>
+                    {isError && <p className={styles.resultTile}>Error in receiving data, try again</p>}
+                    {isLoading && <CoinSearching />}
+                    {search.length > 0 && data && data.data.coins.length === 0 && <p className={styles.resultTile}>Coin not found</p>}
+                    {search.length > 0 && data && data.data.coins.slice(0, 5).map(coin => <CoinSearchTile key={coin.id} coin={coin} />)}
+                </div>
+            }
         </div>
     )
 }
